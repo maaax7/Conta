b0d35c1ad4d52e4bf187866f672d3f604a52172a
Maxwell Santos <maxwell.santos@iterative.com.br>
2016-11-11 14:52:37 -0200

    More Stuffs
    methods: Sacar, Depositar, Transferir e CalcularRendimentoAnual

--- Conta/Conta.cs
+++ Conta/Conta.cs
@@ -13,5 +13,41 @@ namespace Conta
         public int numero { get; set; }
         public int agencia { get; set; }
         public string cpf { get; set; }
+
+        public void Sacar(double valorASacar)
+        {
+            if (valorASacar <= this.saldo && valorASacar >= 0)
+            {
+                this.saldo -= valorASacar;
+            }
+        }
+
+        public void Depositar(double valorADepositar)
+        {
+            if (valorADepositar >= 0)
+            {
+                this.saldo += valorADepositar;
+            }
+        }
+
+        public void Transferir(double valorASerTransferido, Conta destino)
+        {
+            this.Sacar(valorASerTransferido);
+            destino.Depositar(valorASerTransferido);
+        }
+
+        public double CalculaRendimentoAnual()
+        {
+            double saldoNaqueleMes = this.saldo;
+
+            for (int i = 0; i < 12; i++)
+            {
+                saldoNaqueleMes = saldoNaqueleMes * 1.007;
+            }
+
+            double rendimento = saldoNaqueleMes - this.saldo;
+
+            return rendimento;
+        }
     }
 }
