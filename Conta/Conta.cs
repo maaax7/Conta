2a25874c24406dfdb787674385036d6453fbd820
Maxwell Santos <maxwell.santos@iterative.com.br>
2016-11-11 12:34:59 -0200

    Alura Exercises

---
+++ Conta/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta
+{
+    public class Conta
+    {
+        public double saldo { get; set; }
+        public string titular { get; set; }
+        public int numero { get; set; }
+    }
+}
---
+++ Conta/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta conta = new Conta();
+            conta.numero = 1;
+            conta.saldo = 1500.0;
+            conta.titular = "Jabidailsom";
+
+            MessageBox.Show("Numero: " + conta.numero + "\nTitular: " + conta.titular + "\nSaldo: " + conta.saldo);
+        }
+    }
+}
