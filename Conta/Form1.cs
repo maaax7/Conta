804559070f998f81639805d4434b0a49db8d3ce9
Maxwell <maxtuka@gmail.com>
2017-03-08 11:30:03 -0300

    Arrays

--- Conta/Form1.cs
+++ Conta/Form1.cs
@@ -19,25 +19,14 @@ namespace Conta
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Conta c = new Conta();
-            ContaCorrente cc = new ContaCorrente();
-            ContaPoupanca cp = new ContaPoupanca();
+            Conta[] contas = new Conta[5];
+            contas[0] = new Conta();
+            contas[1] = new ContaPoupanca();
 
-            c.Deposita(1000.0);
-            cc.Deposita(1000.0);
-            cp.Deposita(1000.0);
-
-            AtualizadorDeContas atualizador = new AtualizadorDeContas(0.01);
-            atualizador.Roda(c);
-            atualizador.Roda(cc);
-            atualizador.Roda(cp);
-
-            TotalizadorDeContas t = new TotalizadorDeContas();
-            t.Adiciona(cp);
-
-            MessageBox.Show("c = " + c.Saldo);
-            MessageBox.Show("cc = " + cc.Saldo);
-            MessageBox.Show("cp = " + cp.Saldo);
+            foreach (Conta c in contas)
+            {
+                MessageBox.Show(c.Saldo.ToString());
+            }
         }
     }
 }
